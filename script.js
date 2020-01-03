75e4665561f4e3ac7b81744f6e421e5a39dec29a
Emma <Emma@Mathiass-MacBook-Pro.local>
2020-01-03 15:48:00 +0100

    Burger menu virker

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+window.addEventListener("load", sidenVises);
 
+
+function sidenVises() {
+    console.log("siden vises");
+    document.querySelector("#burger_menu").addEventListener("touched", menuOpenClose);
+    document.querySelector("#burger_menu").addEventListener("click", menuOpenClose);
+}
+
+function menuOpenClose() {
+    console.log("menu pressed");
+    this.classList.toggle("burger_kryds");
+    let x = document.querySelector("#mylinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+
+        document.querySelector("#burger_menu").textContent = "☰";
+    } else {
+        x.style.display = "block";
+        document.querySelector("#burger_menu").textContent = "x";
+    }
+}
